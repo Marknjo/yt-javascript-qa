c102cfba446a6fc197017d33ac9fb8088d3b4cd8
Marknjo <marknjo7@gmail.com>
2023-02-16 19:41:27 +0300

    JsQuiz#07

---
+++ src/quizzes/jsquiz-#07.js
@@ -0,0 +1,4 @@
+// JsQuiz#07
+let a = "hello";
+
+console.log(a[1]);
---
+++ src/quizzes/jsquiz-#08.js
@@ -0,0 +1,10 @@
+// JsQuiz#08
+let a = [1, 2, 3];
+
+let b = [...a];
+
+let c = a;
+
+a.pop();
+
+console.log(b === a && c === a);
